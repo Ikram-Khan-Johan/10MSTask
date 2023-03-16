69a2289856f75e42dfefbe19d8000abdeac7e79b
Ikram Khan Johan <ikram.johan@gmail.com>
2023-03-16 15:20:22 +0600

    Initial Commit,Implemented API

---
+++ 10MSTask/MainViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MainViewController.swift
+//  10MSTask
+//
+//  Created by Johan on 16/3/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var products: Products?
+    let baseURL: String = "https://fakestoreapi.com/products"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fetchProductData(url: baseURL) { result in
+            self.products = result
+            
+            print(self.products![2].title )
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func fetchProductData(url: String, completion: @escaping (Products)-> Void){
+        guard let url = URL(string: url) else {
+            print("url String convert failed")
+            return
+        }
+        
+        let session = URLSession.shared
+        let urlRequest = URLRequest(url: url)
+        
+        let dataTask = session.dataTask(with: urlRequest) { data, response, error in
+            
+            guard let data = data else {
+                print("data not found!")
+                return
+            }
+            
+            if error == nil {
+                
+                do {
+                    let parsedData = try JSONDecoder().decode(Products.self, from: data)
+                    completion(parsedData)
+                } catch {
+                    print("parsing error")
+                }
+                
+            } else {
+                print(error?.localizedDescription ?? "Error")
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
---
+++ 10MSTask/Models/Products.swift
@@ -0,0 +1,48 @@
+//
+//  Products.swift
+//  10MSTask
+//
+//  Created by Johan on 16/3/23.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: Category
+    let image: String
+    let rating: Rating
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case price = "price"
+        case description = "description"
+        case category = "category"
+        case image = "image"
+        case rating = "rating"
+    }
+}
+
+enum Category: String, Codable {
+    case electronics = "electronics"
+    case jewelery = "jewelery"
+    case menSClothing = "men's clothing"
+    case womenSClothing = "women's clothing"
+}
+
+// MARK: - Rating
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+
+    enum CodingKeys: String, CodingKey {
+        case rate = "rate"
+        case count = "count"
+    }
+}
+
+typealias Products = [Product]
